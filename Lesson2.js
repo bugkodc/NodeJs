943e805b5e3996cf26b70976a1b99ced581b4818
Luandnq <doluan590@gmail.com>
2024-01-18 07:41:02 +0700

    18/1

---
+++ Lesson2.js
@@ -0,0 +1,25 @@
+var x = prompt("nhap x");
+        x = parseInt(x);
+        switch (x){
+            case 2 :
+                document.writeln("Thu hai");
+                break;
+            case 3 :
+                document.writeln("Thu ba");
+                break;
+            case 4 :
+                document.writeln("Thu tu");
+                break;
+            case 5 :
+                document.writeln("Thu nam");
+                break;
+            case 6 :
+                document.writeln("Thu sau");
+                break;
+            case 7 :
+                document.writeln("Thu bay");
+                break;
+            default :
+                document.writeln("Chu nhat");
+                break;
+            }
